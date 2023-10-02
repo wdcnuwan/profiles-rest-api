e77fdfd67597c02087cae4ab0736b8e1161af996
wdcnuwan <142044490+wdcnuwan@users.noreply.github.com>
2023-10-02 17:49:41 +0530

    USER_PART1

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+
+
+class userprofile(AbstractBaseUser, PermissionsMixin):
+    """Databse model for users in the system"""
+    email = models.EmailField(max_length=255,unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    Is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELD = ['NAME']
+
+
+
+
 
 # Create your models here.
